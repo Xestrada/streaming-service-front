d75ed4c5ea314528c1e3ef0af2f05c498530ce30
Willie Lan <willie1219@gmail.com>
2019-03-01 09:37:48 -0800

    Spacing and indentation resend

--- src/features/common/header.js
+++ src/features/common/header.js
@@ -23,10 +23,11 @@ export default class Header extends React.Component{
     constructor(props) {
         super(props);
     
-        this.toggle = this.toggle.bind(this);
         this.state = {
           isOpen: false
         };
+
+        this.toggle = this.toggle.bind(this);
       }
 
       toggle() {
@@ -39,69 +40,94 @@ export default class Header extends React.Component{
       return (
         <div>
         <Navbar color="dark" light expand="md">
+
           <NavbarBrand className="color-me" href="/">Company 48</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
+
           <Collapse isOpen={this.state.isOpen} navbar>
+
             <Nav className="mr-auto" navbar>
+
               <NavItem >
                 <NavLink className="color-me" href="/components/">Movies</NavLink>
               </NavItem>
+
               <NavItem>
                 <NavLink className="color-me" href="/components/">TV Shows</NavLink>
               </NavItem>
+
               <NavItem>
                 <NavLink className="color-me" href="/components/">Subscription</NavLink>
               </NavItem>
 
               <UncontrolledDropdown nav inNavbar>
+
                 <DropdownToggle className="color-me" nav caret>
                     Genres
                 </DropdownToggle>
+                
                 <DropdownMenu right>
+
                   <DropdownItem>
                     COMEDY
                   </DropdownItem>
+
                   <DropdownItem>
                     SCI-FI
                   </DropdownItem>
+
                   <DropdownItem>
                     HORROR
                   </DropdownItem>
+
                   <DropdownItem>
                     ROMANCE
                   </DropdownItem>
+
                   <DropdownItem>
                     ACTION
                   </DropdownItem>
+
                   <DropdownItem>
                     ANIMATION
                   </DropdownItem>
+
                 </DropdownMenu>
+
               </UncontrolledDropdown>
 
               <NavItem>
                 <NavLink className="color-me" href="/components/">About</NavLink>
               </NavItem>
+
             </Nav>
 
             <Nav className="ml-auto" navbar>
+
               <UncontrolledDropdown nav inNavbar>
+
                 <DropdownToggle className="color-me" nav caret>
                   Login
                 </DropdownToggle>
+
                 <DropdownMenu right>
                     <Form inline>
+
                         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                             <Label for="exampleEmail" className="mr-sm-2">Email</Label>
                             <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
                         </FormGroup>
+
                         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                              <Label for="examplePassword" className="mr-sm-2">Password</Label>
                              <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
                         </FormGroup>
+
                     <Button>Submit</Button>
+
                     </Form>
                 </DropdownMenu>
+
               </UncontrolledDropdown>
 
               <NavItem>
@@ -111,7 +137,9 @@ export default class Header extends React.Component{
               </Nav>
 
           </Collapse>
+          
         </Navbar>
+        
         </div>
       );
     }
