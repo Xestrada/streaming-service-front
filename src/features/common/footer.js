dd87b32f7155db29a162f032ec696f9f53fc5f8b
Isaac Kim <isaackim97@gmail.com>
2019-03-01 09:45:13 -0800

    removed unused class

--- src/features/common/footer.js
+++ src/features/common/footer.js
@@ -31,7 +31,7 @@ export default class Example extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-        <NavbarBrand className="Navbar" href="/">Contact us at your@email.com or XXX-XXX-XXXX</NavbarBrand>
+        <NavbarBrand href="/">Contact us at your@email.com or XXX-XXX-XXXX</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
