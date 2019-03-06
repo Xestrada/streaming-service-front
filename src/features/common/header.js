f7275b7f2aadf311758b1ad116d360b21c39ea0f
Wei-Hao Lan <willie1219@gmail.com>
2019-03-05 18:02:52 -0800

    Fixed all the linter issues in header.js

--- src/features/common/header.js
+++ src/features/common/header.js
@@ -11,136 +11,137 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
-    Button, 
-    Form, 
-    FormGroup, 
-    Label, 
-    Input
-     } from 'reactstrap';
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+} from 'reactstrap';
 import './header.scss';
 
-export default class Header extends React.Component{
+export default class Header extends React.Component {
     constructor(props) {
         super(props);
-    
+
         this.state = {
-          isOpen: false
+            isOpen: false,
         };
 
         this.toggle = this.toggle.bind(this);
-      }
+    }
 
     toggle() {
-        this.setState({
-          isOpen: !this.state.isOpen
-        });
-      }
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen,
+        }));
+    }
 
     render() {
-      return (
-        <div>
-        <Navbar color="dark" light expand="md">
+        const { isOpen } = this.state;
+        return (
+            <div>
+                <Navbar color='dark' light expand='md'>
 
-          <NavbarBrand className="color-me" href="/">Company 48</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
+                    <NavbarBrand className='color-me' href='/'>Company 48</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
 
-          <Collapse isOpen={this.state.isOpen} navbar>
+                    <Collapse isOpen={isOpen} navbar>
 
-            <Nav className="mr-auto" navbar>
+                        <Nav className='mr-auto' navbar>
 
-              <NavItem >
-                <NavLink className="color-me" href="/components/">Movies</NavLink>
-              </NavItem>
+                            <NavItem>
+                                <NavLink className='color-me' href='/components/'>Movies</NavLink>
+                            </NavItem>
 
-              <NavItem>
-                <NavLink className="color-me" href="/components/">TV Shows</NavLink>
-              </NavItem>
+                            <NavItem>
+                                <NavLink className='color-me' href='/components/'>TV Shows</NavLink>
+                            </NavItem>
 
-              <NavItem>
-                <NavLink className="color-me" href="/components/">Subscription</NavLink>
-              </NavItem>
+                            <NavItem>
+                                <NavLink className='color-me' href='/components/'>Subscription</NavLink>
+                            </NavItem>
 
-              <UncontrolledDropdown nav inNavbar>
+                            <UncontrolledDropdown nav inNavbar>
 
-                <DropdownToggle className="color-me" nav caret>
+                                <DropdownToggle className='color-me' nav caret>
                     Genres
-                </DropdownToggle>
-                
-                <DropdownMenu right>
+                                </DropdownToggle>
 
-                  <DropdownItem>
+                                <DropdownMenu right>
+
+                                    <DropdownItem>
                     COMEDY
-                  </DropdownItem>
+                                    </DropdownItem>
 
-                  <DropdownItem>
+                                    <DropdownItem>
                     SCI-FI
-                  </DropdownItem>
+                                    </DropdownItem>
 
-                  <DropdownItem>
+                                    <DropdownItem>
                     HORROR
-                  </DropdownItem>
+                                    </DropdownItem>
 
-                  <DropdownItem>
+                                    <DropdownItem>
                     ROMANCE
-                  </DropdownItem>
+                                    </DropdownItem>
 
-                  <DropdownItem>
+                                    <DropdownItem>
                     ACTION
-                  </DropdownItem>
+                                    </DropdownItem>
 
-                  <DropdownItem>
+                                    <DropdownItem>
                     ANIMATION
-                  </DropdownItem>
+                                    </DropdownItem>
 
-                </DropdownMenu>
+                                </DropdownMenu>
 
-              </UncontrolledDropdown>
+                            </UncontrolledDropdown>
 
-              <NavItem>
-                <NavLink className="color-me" href="/components/">About</NavLink>
-              </NavItem>
+                            <NavItem>
+                                <NavLink className='color-me' href='/components/'>About</NavLink>
+                            </NavItem>
 
-            </Nav>
+                        </Nav>
 
-            <Nav className="ml-auto" navbar>
+                        <Nav className='ml-auto' navbar>
 
-              <UncontrolledDropdown nav inNavbar>
+                            <UncontrolledDropdown nav inNavbar>
 
-                <DropdownToggle className="color-me" nav caret>
+                                <DropdownToggle className='color-me' nav caret>
                   Login
-                </DropdownToggle>
+                                </DropdownToggle>
 
-                <DropdownMenu right>
-                    <Form inline>
+                                <DropdownMenu right>
+                                    <Form inline>
 
-                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                            <Label for="exampleEmail" className="mr-sm-2">Email</Label>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
-                        </FormGroup>
+                                        <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                                            <Label for='exampleEmail' className='mr-sm-2'>Email</Label>
+                                            <Input type='email' name='email' id='exampleEmail' placeholder='something@idk.cool' />
+                                        </FormGroup>
 
-                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                             <Label for="examplePassword" className="mr-sm-2">Password</Label>
-                             <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
-                        </FormGroup>
+                                        <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                                            <Label for='examplePassword' className='mr-sm-2'>Password</Label>
+                                            <Input type='password' name='password' id='examplePassword' placeholder="don't tell!" />
+                                        </FormGroup>
 
-                    <Button>Submit</Button>
+                                        <Button>Submit</Button>
 
-                    </Form>
-                </DropdownMenu>
+                                    </Form>
+                                </DropdownMenu>
 
-              </UncontrolledDropdown>
+                            </UncontrolledDropdown>
 
-              <NavItem>
-                <NavLink className="color-me" href="/components/">Sign Up</NavLink>
-              </NavItem>
+                            <NavItem>
+                                <NavLink className='color-me' href='/components/'>Sign Up</NavLink>
+                            </NavItem>
 
-              </Nav>
+                        </Nav>
 
-          </Collapse>
-          
-        </Navbar>
-        
-        </div>
-      );
+                    </Collapse>
+
+                </Navbar>
+
+            </div>
+        );
     }
-  }
+}
