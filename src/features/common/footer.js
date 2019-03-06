0074da12353c6570c515e7db53718ac918d7b5f3
Isaac Kim <isaackim97@gmail.com>
2019-03-06 07:26:13 -0800

    fixed all linter issues in footer.js

--- src/features/common/footer.js
+++ src/features/common/footer.js
@@ -1,56 +1,59 @@
 import React from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Media} from 'reactstrap';
-import img from '../../imagesJPEG_example_flower.jpg';
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Media,
+} from 'reactstrap';
+import img from '../../images/JPEG_example_flower.jpg';
 import './footer.scss';
 
 
 export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-  
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Contact us at your@email.com or XXX-XXX-XXXX</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <Media href="https://facebook.com">
-                <Media object src= {img} className='img' alt="Generic placeholder image" />
-              </Media>
-              <Media href="https://twitter.com">
-                <Media object src= {img} className='img' alt="Generic placeholder image" />
-              </Media>
-              <Media href="https://google.com">
-                <Media object src= {img} className='img' alt="Generic placeholder image" />
-              </Media>
-              <NavItem>
-                <NavLink href="/privacyPolicy">Privacy Policy</NavLink>
-              </NavItem> 
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        };
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen,
+        }));
+    }
+
+
+    render() {
+        const { isOpen } = this.state;
+        return (
+            <div>
+                <Navbar color='dark' light expand='md'>
+                    <NavbarBrand href='/'>Contact us at your@email.com or XXX-XXX-XXXX</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className='ml-auto' navbar>
+                            <Media href='https://facebook.com'>
+                                <Media object src={img} className='img' alt='Generic placeholder image' />
+                            </Media>
+                            <Media href='https://twitter.com'>
+                                <Media object src={img} className='img' alt='Generic placeholder image' />
+                            </Media>
+                            <Media href='https://google.com'>
+                                <Media object src={img} className='img' alt='Generic placeholder image' />
+                            </Media>
+                            <NavItem>
+                                <NavLink href='/privacyPolicy'>Privacy Policy</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
