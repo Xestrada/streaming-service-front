5f8f90ec8858eda10407061823095bb241c4978d
Wei-Hao Lan <willie1219@gmail.com>
2019-03-06 18:21:29 -0800

    Fixed the login box spacing. Added !important at the .container padding-left in the header.scss. In order to make the login box move to left have to override the original content style sheet.

--- src/features/common/header.js
+++ src/features/common/header.js
@@ -99,46 +99,46 @@ export default class Header extends React.Component {
                         </Nav>
 
                         <Nav className='ml-auto' navbar>
+                            <Nav className='spacing'>
+                                <Nav className='searchbar'>
+                                    <input className='search_input' type='text' name='' placeholder='Search...' />
+                                    <a href='/' className='search_icon'><i className='fas fa-search' /></a>
+                                </Nav>
+                                <UncontrolledDropdown nav inNavbar>
 
-                            <Nav className='searchbar'>
-                                <input className='search_input' type='text' name='' placeholder='Search...' />
-                                <a href='/' className='search_icon'><i className='fas fa-search' /></a>
-                            </Nav>
-                            <UncontrolledDropdown nav inNavbar>
-
-                                <DropdownToggle className='color-me' nav caret>
+                                    <DropdownToggle className='color-me' nav caret>
                   Login
-                                </DropdownToggle>
+                                    </DropdownToggle>
 
-                                <DropdownMenu background='blue' right>
-                                    <div className='container'>
-                                        <div className='row'>
-                                            <div className='col-md-offset-5 col-md-3'>
-                                                <div className='form-login'>
-                                                    <h4>Welcome back.</h4>
-                                                    <input type='text' id='userName' className='form-control input-sm chat-input' placeholder='username' />
-                                                    <input type='text' id='userPassword' className='form-control input-sm chat-input' placeholder='password' />
-                                                    <div className='wrapper'>
-                                                        <span className='group-btn'>
-                                                            <a href='/' className='btn btn-primary btn-md'>
+                                    <DropdownMenu style={{ padding: 0 }} right>
+                                        <div className='container'>
+                                            <div className='row'>
+                                                <div className='col-md-offset-5 col-md-3'>
+                                                    <div className='form-login'>
+                                                        <h4>Welcome back.</h4>
+                                                        <input type='text' id='userName' className='form-control input-sm chat-input' placeholder='username' />
+                                                        <input type='text' id='userPassword' className='form-control input-sm chat-input' placeholder='password' />
+                                                        <div className='wrapper'>
+                                                            <span className='group-btn'>
+                                                                <a href='/' className='btn btn-primary btn-md'>
 login
-                                                                {' '}
-                                                                <i className='fas fa-sign-in-alt' />
-                                                            </a>
-                                                        </span>
+                                                                    {' '}
+                                                                    <i className='fas fa-sign-in-alt' />
+                                                                </a>
+                                                            </span>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </DropdownMenu>
+                                    </DropdownMenu>
 
-                            </UncontrolledDropdown>
+                                </UncontrolledDropdown>
 
-                            <NavItem>
-                                <NavLink className='color-me' href='/components/'>Sign Up</NavLink>
-                            </NavItem>
-
+                                <NavItem>
+                                    <NavLink className='color-me' href='/components/'>Sign Up</NavLink>
+                                </NavItem>
+                            </Nav>
                         </Nav>
 
                     </Collapse>
