876177b48e0a1e44b0f712a38317fefb54aaf621
Willie Lan <willie1219@gmail.com>
2019-03-10 10:06:09 -0700

    Added the SearchBar component

--- src/features/common/header.js
+++ src/features/common/header.js
@@ -11,9 +11,9 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
-    Input,
 } from 'reactstrap';
 import './header.scss';
+import SearchBar from './SearchBar';
 
 export default class Header extends React.Component {
     constructor(props) {
@@ -154,28 +154,7 @@ login
                     </Collapse>
 
                 </Navbar>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-xs-8 col-xs-offset-2'>
-                            <div className='input-group'>
-                                <div className='input-group-btn search-panel'>
-                                    <Input type='select' name='select' id='exampleSelect'>
-                                        <option>All</option>
-                                        <option>Title</option>
-                                        <option>Genre</option>
-                                        <option>Year</option>
-                                        <option>Actor</option>
-                                    </Input>
-                                </div>
-                                <input type='hidden' name='search_param' value='all' id='search_param' />
-                                <input type='text' className='form-control' name='x' placeholder='Search...' />
-                                <span className='input-group-btn'>
-                                    <button className='btn btn-primary' type='submit'><i className='fas fa-search' /></button>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <SearchBar />
             </div>
         );
     }
