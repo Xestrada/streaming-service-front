c0c2e7c4b5189f508a831e14bbdf6d4d65fb7c49
Jorge Estrada <Xestrada.Jorge@gmail.com>
2019-03-13 18:39:35 -0700

    Created tv shows page

--- src/features/common/redux/tvShows.js
+++ src/features/common/redux/tvShows.js
@@ -7,19 +7,26 @@ import {
 
 // Rekit uses redux-thunk for async actions by default: https://github.com/gaearon/redux-thunk
 // If you prefer redux-saga, you can use rekit-plugin-redux-saga: https://github.com/supnate/rekit-plugin-redux-saga
-export function getTV() {
+export function getTV(pageNum) {
     return (dispatch) => { // optionally you can have getState as the second argument
         dispatch({
             type: COMMON_TV_SHOWS_BEGIN,
         });
 
 
-        return fetch('https://ss-media-middle.herokuapp.com/tv_shows')
-            .then(response => response.json())
+        return fetch(`https://ss-media-middle.herokuapp.com/tv_shows/recently_added/page=${pageNum}`)
+            .then((response) => {
+                dispatch({
+                    type: 'Header Data',
+                    maxPages: response.headers.get('max_pages'),
+                });
+                return response.json();
+            },
+            )
             .then((createdJson) => {
                 dispatch({
                     type: COMMON_TV_SHOWS_SUCCESS,
-                    data: createdJson,
+                    data: createdJson.tv_shows,
                 });
             })
             .catch((error) => {
@@ -29,6 +36,37 @@ export function getTV() {
                 });
                 console.log('Error: ', error);
             });
+    };
+}
+
+export function searchTV(filter, query, pageNum) {
+    return (dispatch) => { // optionally you can have getState as the second argument
+        dispatch({
+            type: COMMON_TV_SHOWS_BEGIN,
+        });
+        return fetch(`https://ss-media-middle.herokuapp.com/tv_shows/${filter}=${query}/page=${pageNum}`)
+            .then((response) => {
+                dispatch({
+                    type: 'Header Data',
+                    maxPages: response.headers.get('max_pages'),
+                });
+                return response.json();
+            },
+            )
+            .then((createdJson) => {
+                dispatch({
+                    type: COMMON_TV_SHOWS_SUCCESS,
+                    data: createdJson.tv_shows,
+                });
+            })
+            .catch((error) => {
+                dispatch({
+                    type: COMMON_TV_SHOWS_FAILURE,
+                    data: error,
+                });
+                console.log('Error: ', error);
+            });
+
     };
 }
 
@@ -56,7 +94,7 @@ export function reducer(state, action) {
             ...state,
             tvShowsPending: false,
             tvShowsError: null,
-            tvShows: action.data.tv_shows,
+            tvShows: action.data,
         };
 
     case COMMON_TV_SHOWS_FAILURE:
@@ -72,6 +110,11 @@ export function reducer(state, action) {
         return {
             ...state,
             tvShowsError: null,
+        };
+    case 'Header Data':
+        return {
+            ...state,
+            maxPages: action.maxPages,
         };
 
     default:
