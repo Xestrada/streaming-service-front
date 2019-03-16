383d9c25e19ef74fd1b95ffe4b2a2a0bc9a2ae50
Jorge Estrada <Xestrada.Jorge@gmail.com>
2019-03-15 20:51:41 -0700

    Header reflects login state

--- src/features/common/header.js
+++ src/features/common/header.js
@@ -5,10 +5,10 @@ import {
     Collapse,
     Navbar,
     NavbarToggler,
-    NavLink,
     Nav,
     NavItem,
     Button,
+    Media,
     Modal,
     ModalHeader,
     ModalBody,
@@ -18,6 +18,7 @@ import { Link } from 'react-router-dom';
 import './header.scss';
 import { connect } from 'react-redux';
 import * as actions from './redux/actions';
+import userImg from '../../images/blank-user.jpg';
 
 export class Header extends React.Component {
     static propTypes = {
@@ -64,10 +65,47 @@ export class Header extends React.Component {
         const { isOpen } = this.state;
         const { modal } = this.state;
         const { common } = this.props;
-        console.log(common.authen);
+
+        const modalElem = (
+            <Modal isOpen={modal} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggle}><h2 className='centerModalHeader'>Member Login</h2></ModalHeader>
+                <ModalBody className='modalBody'>
+                    <input type='text' id='userName' className='form-control' placeholder='username' />
+                    <input type='password' id='userPassword' className='form-control input-sm chat-input' placeholder='password' />
+                    <label>
+                        <input type='checkbox' name='remember' value='1' />
+                        <span className='remember'>Remember me</span>
+                    </label>
+                </ModalBody>
+                <ModalFooter>
+                    <Button className='btn btn-primary btn-md' color='primary' onClick={this.login}>
+                        login
+                        <i className='fas fa-sign-in-alt' />
+                    </Button>
+                    <Button color='secondary' onClick={this.toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        );
+
+        const sideHead = common.authen ? (
+            <Media right>
+                <img className='user' src={userImg} alt='Placeholder' />
+            </Media>
+        ) : (
+            <div>
+                <Button className='color-me link' color='white' onClick={this.toggle}>
+                Login
+                </Button>
+
+                <Link className='color-me link' to='/signup'>Sign Up</Link>
+            </div>
+        );
 
         return (
             <div>
+
+                {modalElem}
+
                 <Navbar color='dark' light expand='md'>
 
                     <Link className='color-me brand' to='/'>Company 48</Link>
@@ -96,33 +134,10 @@ export class Header extends React.Component {
 
                         </Nav>
                         <Nav className='ml-auto' navbar>
-                            <Nav className='spacing'>
+                            <Nav className='spacing' />
 
-                                <Button className='color-me link' color='white' onClick={this.toggle}>
-                                    Login
-                                </Button>
-                                <Modal isOpen={modal} toggle={this.toggle}>
-                                    <ModalHeader toggle={this.toggle}><h2 className='centerModalHeader'>Member Login</h2></ModalHeader>
-                                    <ModalBody className='modalBody'>
-                                        <input type='text' id='userName' className='form-control' placeholder='username' />
-                                        <input type='password' id='userPassword' className='form-control input-sm chat-input' placeholder='password' />
-                                        <label>
-                                            <input type='checkbox' name='remember' value='1' />
-                                            <span className='remember'>Remember me</span>
-                                        </label>
-                                    </ModalBody>
-                                    <ModalFooter>
-                                        <Button className='btn btn-primary btn-md' color='primary' onClick={this.login}>
-                                                login
-                                            <i className='fas fa-sign-in-alt' />
-                                        </Button>
-                                        <Button color='secondary' onClick={this.toggle}>Cancel</Button>
-                                    </ModalFooter>
-                                </Modal>
+                            {sideHead}
 
-                                <Link className='color-me link' to='/signup'>Sign Up</Link>
-                                
-                            </Nav>
                         </Nav>
 
                     </Collapse>
