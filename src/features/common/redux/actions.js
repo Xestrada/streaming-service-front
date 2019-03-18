3286b6ce8fdf6673b664dc301156ad5d2f8d3dc5
Jorge Estrada <Xestrada.Jorge@gmail.com>
2019-03-18 08:32:53 -0700

    Fixed actions.js export issue

--- src/features/common/redux/actions.js
+++ src/features/common/redux/actions.js
@@ -3,3 +3,4 @@ export { searchMovies, getMovies, dismissMoviesError } from './movies';
 export { searchTV, getTV, dismissTvShowsError } from './tvShows';
 export { getMedia, dismissMediaError } from './media';
 export { authen, signOut, dismissAuthenError } from './authen';
+export { getRecents, searchBy } from './search';
