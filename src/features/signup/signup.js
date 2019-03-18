cfb9534febb25d9d74c71809cdc0a4099f8734ef
Jorge Estrada <Xestrada.Jorge@gmail.com>
2019-03-18 10:32:44 -0700

    Added value storage to form

--- src/features/signup/signup.js
+++ src/features/signup/signup.js
@@ -3,12 +3,40 @@ import { Navbar, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import Header from '../common/header';
 import Footer from '../common/footer';
 import './signup.scss';
-import { applyMiddleware } from '../../../../../AppData/Local/Microsoft/TypeScript/3.3/node_modules/@types/redux-logger/node_modules/redux';
 
 class Signup extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            username: '',
+            pass: '',
+            cc: '',
+            exp: '',
+        }
+
+        this.changeState = this.changeState.bind(this);
+        this.signUp = this.signUp.bind(this);
+    }
+
+    changeState(name, value) {
+        this.setState({
+            [name] : value,
+        });
+    }
+
+    signUp() {
+
+    }
+
 
     render() {
+
+        const {username, pass, email, cc, exp} = this.state;
+
+
         return (
             <div>
                 <Header />
@@ -20,37 +48,31 @@ class Signup extends Component {
                     <FormGroup row>
                         <Label for='exampleEmail' sm={3}>Email</Label>
                         <Col sm={8}>
-                            <Input type='email' name='email' id='exampleEmail' placeholder='your email' />
+                            <Input value={email} type='email' name='email' id='exampleEmail' placeholder='your email' onChange={e => this.changeState('email', e.target.value)}/>
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label for='exampleUsername' sm={3}>Username</Label>
                         <Col sm={8}>
-                            <Input type='username' name='username' id='exampleUsername' placeholder='your username' />
+                            <Input value={username}type='username' name='username' id='exampleUsername' placeholder='your username' onChange={e => this.changeState('username', e.target.value)} />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label for='examplePassword' sm={3}>Password</Label>
                         <Col sm={9}>
-                            <Input type='password' name='password' id='examplePassword' placeholder='your password' />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for='exampleRePassword' sm={3}>Re-Password</Label>
-                        <Col sm={9}>
-                            <Input type='password' name='password' id='examplePassword' placeholder='retype password' />
+                            <Input value={pass}type='password' name='password' id='examplePassword' placeholder='your password' onChange={e => this.changeState('pass', e.target.value)} />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label for='exampleCreditCardID' sm={3}>Credit Card Number</Label>
                         <Col sm={9}>
-                            <Input type='creditcardID' name='creditcardID' id='exampleCreditcardID' placeholder='your credit card #' />
+                            <Input value={cc} type='creditcardID' name='creditcardID' id='exampleCreditcardID' placeholder='your credit card #' onChange={e => this.changeState('cc', e.target.value)} />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label for='exampleCreditCardExp' sm={3}>Credit Card Exp</Label>
                         <Col sm={9}>
-                            <Input type='creditcardExp' name='creditcardExp' id='exampleCreditcardExp' placeholder='your credit card exp' />
+                            <Input value={exp} type='creditcardExp' name='creditcardExp' id='exampleCreditcardExp' placeholder='your credit card exp' onChange={e => this.changeState('exp', e.target.value)} />
                         </Col>
                     </FormGroup>
                     <FormGroup check row>
