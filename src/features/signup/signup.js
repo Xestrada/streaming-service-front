0ddc29ec126a9c30f890e4fd807cb55b3ed4a71e
Isaac Kim <isaackim97@gmail.com>
2019-03-19 08:15:48 -0700

    sign up eslint issues and added privacy page

--- src/features/signup/signup.js
+++ src/features/signup/signup.js
@@ -5,55 +5,61 @@ import Footer from '../common/footer';
 import './signup.scss';
 
 class Signup extends Component {
-
-
     render() {
         return (
             <div>
                 <Header />
-                <Form>
-                    <FormGroup row>
-                        <Label for='exampleEmail' sm={2}>Email</Label>
-                        <Col sm={10}>
-                            <Input type='email' name='email' id='exampleEmail' placeholder='your email' />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for='exampleUsername' sm={2}>Username</Label>
-                        <Col sm={10}>
-                            <Input type='username' name='username' id='exampleUsername' placeholder='your username' />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for='examplePassword' sm={2}>Password</Label>
-                        <Col sm={10}>
-                            <Input type='password' name='password' id='examplePassword' placeholder='your password' />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for='exampleRePassword' sm={2}>Re-Password</Label>
-                        <Col sm={10}>
-                            <Input type='password' name='password' id='examplePassword' placeholder='your password' />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for='exampleCreditCardID' sm={2}>Credit Card Number</Label>
-                        <Col sm={10}>
-                            <Input type='creditcardID' name='creditcardID' id='exampleCreditcardID' placeholder='your creditcardID' />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for='exampleCreditCardExp' sm={2}>Credit Card Exp</Label>
-                        <Col sm={10}>
-                            <Input type='creditcardExp' name='creditcardExp' id='exampleCreditcardExp' placeholder='your creditcardExp' />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup check row>
-                        <Col sm={{ size: 10, offset: 2 }}>
-                            <Button>Submit</Button>
-                        </Col>
-                    </FormGroup>
-                </Form>
+                <div className='middle'>
+                    <div className='coloring'>
+                        {'Create your account'}
+                    </div>
+                    <Form>
+                        <h1>
+                            {' '}
+                        </h1>
+                        <FormGroup row>
+                            <Label for='exampleEmail' sm={3}>Email</Label>
+                            <Col sm={8}>
+                                <Input type='email' name='email' id='exampleEmail' placeholder='your email' />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for='exampleUsername' sm={3}>Username</Label>
+                            <Col sm={8}>
+                                <Input type='username' name='username' id='exampleUsername' placeholder='your username' />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for='examplePassword' sm={3}>Password</Label>
+                            <Col sm={9}>
+                                <Input type='password' name='password' id='examplePassword' placeholder='your password' />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for='exampleRePassword' sm={3}>Re-Password</Label>
+                            <Col sm={9}>
+                                <Input type='password' name='password' id='examplePassword' placeholder='retype password' />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for='exampleCreditCardID' sm={3}>Credit Card Number</Label>
+                            <Col sm={9}>
+                                <Input type='creditcardID' name='creditcardID' id='exampleCreditcardID' placeholder='your credit card #' />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for='exampleCreditCardExp' sm={3}>Credit Card Exp</Label>
+                            <Col sm={9}>
+                                <Input type='creditcardExp' name='creditcardExp' id='exampleCreditcardExp' placeholder='your credit card exp' />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup check row>
+                            <Col sm={{ size: 10, offset: 5 }}>
+                                <Button>Submit</Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </div>
                 <Footer />
             </div>
         );
