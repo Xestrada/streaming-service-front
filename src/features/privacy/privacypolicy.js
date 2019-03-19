a0e6e641a014651891855bc59fa8a50d83d7c3b4
Isaac Kim <isaackim97@gmail.com>
2019-03-19 13:11:49 -0700

    adding privacy policy

--- src/features/privacy/privacypolicy.js
+++ src/features/privacy/privacypolicy.js
@@ -5,12 +5,51 @@ import './privacypolicy.scss';
 
 class PrivacyPolicy extends Component {
 
-
     render() {
         return (
             <div>
                 <Header />
-                We're gonna sell all your data lol
+                <div className='title'>
+                    {'Privacy Policy'}
+                </div>
+                <div className='paragraph'>
+                    Lorem ipsum dolor sit amet, pro eius molestiae referrentur ne. At illum antiopam evertitur vix. Ipsum deseruisse te sit, doming aliquip accusamus eam ea, nec ut movet fierent. An mei iriure meliore dissentias, ex homero soleat iudicabit ius. Mel cu odio voluptua, recusabo perpetua ut eos, malis perpetua electram ne nam.
+                    Eu recusabo ocurreret usu, no vel saepe feugait. Est oratio scribentur ne, mel cetero viderer ullamcorper no, mel ne congue aperiam fastidii. Nominavi disputando no per, et feugiat deleniti placerat pri, dicant fabellas suavitate vix ei. Ut ferri etiam voluptatibus pri, convenire mediocrem definitiones at usu.
+                    Ut est probo aperiri inciderint, eu legimus alienum theophrastus pro.
+                </div>
+                <div>
+                    <a href='#terms'>Terms and Conditions</a>
+                </div>
+                <div>
+                    <a href='#public'>Public Information</a>
+                </div>
+                <div>
+                    <a href='#payment'>Payment Information</a>
+                </div>
+                <div className='title' name='terms'>
+                    {'Terms and Conditions'}
+                </div>
+                <div className='paragraph'>
+                    Lorem ipsum dolor sit amet, pro eius molestiae referrentur ne. At illum antiopam evertitur vix. Ipsum deseruisse te sit, doming aliquip accusamus eam ea, nec ut movet fierent. An mei iriure meliore dissentias, ex homero soleat iudicabit ius. Mel cu odio voluptua, recusabo perpetua ut eos, malis perpetua electram ne nam.
+                    Eu recusabo ocurreret usu, no vel saepe feugait. Est oratio scribentur ne, mel cetero viderer ullamcorper no, mel ne congue aperiam fastidii. Nominavi disputando no per, et feugiat deleniti placerat pri, dicant fabellas suavitate vix ei. Ut ferri etiam voluptatibus pri, convenire mediocrem definitiones at usu.
+                    Ut est probo aperiri inciderint, eu legimus alienum theophrastus pro.
+                </div>
+                <div className='title' name='public'>
+                    {'Public Information'}
+                </div>
+                <div className='paragraph'>
+                    Lorem ipsum dolor sit amet, pro eius molestiae referrentur ne. At illum antiopam evertitur vix. Ipsum deseruisse te sit, doming aliquip accusamus eam ea, nec ut movet fierent. An mei iriure meliore dissentias, ex homero soleat iudicabit ius. Mel cu odio voluptua, recusabo perpetua ut eos, malis perpetua electram ne nam.
+                    Eu recusabo ocurreret usu, no vel saepe feugait. Est oratio scribentur ne, mel cetero viderer ullamcorper no, mel ne congue aperiam fastidii. Nominavi disputando no per, et feugiat deleniti placerat pri, dicant fabellas suavitate vix ei. Ut ferri etiam voluptatibus pri, convenire mediocrem definitiones at usu.
+                    Ut est probo aperiri inciderint, eu legimus alienum theophrastus pro.
+                </div>
+                <div className='title' name='payment'>
+                    {'Payment Information'}
+                </div>
+                <div className='paragraph'>
+                    Lorem ipsum dolor sit amet, pro eius molestiae referrentur ne. At illum antiopam evertitur vix. Ipsum deseruisse te sit, doming aliquip accusamus eam ea, nec ut movet fierent. An mei iriure meliore dissentias, ex homero soleat iudicabit ius. Mel cu odio voluptua, recusabo perpetua ut eos, malis perpetua electram ne nam.
+                    Eu recusabo ocurreret usu, no vel saepe feugait. Est oratio scribentur ne, mel cetero viderer ullamcorper no, mel ne congue aperiam fastidii. Nominavi disputando no per, et feugiat deleniti placerat pri, dicant fabellas suavitate vix ei. Ut ferri etiam voluptatibus pri, convenire mediocrem definitiones at usu.
+                    Ut est probo aperiri inciderint, eu legimus alienum theophrastus pro.
+                </div>
                 <Footer />
             </div>
         );
