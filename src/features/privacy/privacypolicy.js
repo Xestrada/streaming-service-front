ce9053636cb2e8f2cb9714ce4ce8381315cef5b2
Isaac Kim <isaackim97@gmail.com>
2019-03-19 18:18:59 -0700

    finish privacy policy

--- src/features/privacy/privacypolicy.js
+++ src/features/privacy/privacypolicy.js
@@ -9,24 +9,16 @@ class PrivacyPolicy extends Component {
         return (
             <div>
                 <Header />
-                <div className='title'>
+                <div className='maintitle'>
                     {'Privacy Policy'}
                 </div>
-                <div className='paragraph'>
+                <div className='mainparagraph'>
                     Lorem ipsum dolor sit amet, pro eius molestiae referrentur ne. At illum antiopam evertitur vix. Ipsum deseruisse te sit, doming aliquip accusamus eam ea, nec ut movet fierent. An mei iriure meliore dissentias, ex homero soleat iudicabit ius. Mel cu odio voluptua, recusabo perpetua ut eos, malis perpetua electram ne nam.
                     Eu recusabo ocurreret usu, no vel saepe feugait. Est oratio scribentur ne, mel cetero viderer ullamcorper no, mel ne congue aperiam fastidii. Nominavi disputando no per, et feugiat deleniti placerat pri, dicant fabellas suavitate vix ei. Ut ferri etiam voluptatibus pri, convenire mediocrem definitiones at usu.
                     Ut est probo aperiri inciderint, eu legimus alienum theophrastus pro.
                 </div>
-                <div>
-                    <a href='#terms'>Terms and Conditions</a>
-                </div>
-                <div>
-                    <a href='#public'>Public Information</a>
-                </div>
-                <div>
-                    <a href='#payment'>Payment Information</a>
-                </div>
-                <div className='title' name='terms'>
+
+                <div className='paragraphtitle'>
                     {'Terms and Conditions'}
                 </div>
                 <div className='paragraph'>
@@ -34,7 +26,7 @@ class PrivacyPolicy extends Component {
                     Eu recusabo ocurreret usu, no vel saepe feugait. Est oratio scribentur ne, mel cetero viderer ullamcorper no, mel ne congue aperiam fastidii. Nominavi disputando no per, et feugiat deleniti placerat pri, dicant fabellas suavitate vix ei. Ut ferri etiam voluptatibus pri, convenire mediocrem definitiones at usu.
                     Ut est probo aperiri inciderint, eu legimus alienum theophrastus pro.
                 </div>
-                <div className='title' name='public'>
+                <div className='paragraphtitle'>
                     {'Public Information'}
                 </div>
                 <div className='paragraph'>
@@ -42,7 +34,7 @@ class PrivacyPolicy extends Component {
                     Eu recusabo ocurreret usu, no vel saepe feugait. Est oratio scribentur ne, mel cetero viderer ullamcorper no, mel ne congue aperiam fastidii. Nominavi disputando no per, et feugiat deleniti placerat pri, dicant fabellas suavitate vix ei. Ut ferri etiam voluptatibus pri, convenire mediocrem definitiones at usu.
                     Ut est probo aperiri inciderint, eu legimus alienum theophrastus pro.
                 </div>
-                <div className='title' name='payment'>
+                <div className='paragraphtitle'>
                     {'Payment Information'}
                 </div>
                 <div className='paragraph'>
