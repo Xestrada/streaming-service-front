b6ae04a1a721591cb15948983e6531917020feb9
Willie Lan <willie1219@gmail.com>
2019-03-25 13:21:56 -0700

    Added the information for stars and genres but still need figure out how to add comma in between. Also changed the layout of the media page where the cover image resize smaller and video player box resize bigger.

--- src/features/media/media.js
+++ src/features/media/media.js
@@ -40,18 +40,21 @@ export class Media extends Component {
   render() {
       const { title } = this.state;
       const { common } = this.props;
-      const { media, mediaError, authen } = common;
-
+      const { media, mediaError } = common;
+      console.log(media);
       const seasonInfo = (media !== undefined && media.season_info !== undefined) ? media.season_info.map((content) => {
-
           const episodeInfo = (content.episodes !== undefined) ? content.episodes.map(item => (
               <div>
                   <span>
                       <li>
-                        Episode
+Episode
+                          {' '}
                           {item.episode}
-                        :
+:
+                          {' '}
+                          {' '}
                           {item.episode_name}
+                          {' '}
                       </li>
 
                   </span>
@@ -61,9 +64,10 @@ export class Media extends Component {
           return (
               <div>
                   {' '}
-                  <Button className='season-position' onClick={this.toggleHidden} id='toggler' style={{ textAlign: 'left', marginBottom: '1rem' }}>
+                  <Button className='season-position' id='toggler' style={{ textAlign: 'left', marginBottom: '1rem' }}>
                       <li>
-                        Season:
+Season:
+                          {' '}
                           {content.season}
                       </li>
                   </Button>
@@ -72,23 +76,10 @@ export class Media extends Component {
                   </UncontrolledCollapse>
               </div>
           );
-
       }) : null;
-
-      const commentContainer = authen ? (
-          <div className='comment-container'>
-              <div className='comment-header'>
-                  <label htmlFor='Comment' style={{ textDecoration: 'underline', fontFamily: 'Apple Chancery, cursive' }}>Leave a comment</label>
-              </div>
-              <textarea id='subject' name='subject' placeholder='Enter your comment here...' style={{ borderStyle: 'inset', width: '600px', height: '90px' }} />
-              <div className='row'>
-                  <input type='submit' value='Post Comment' />
-              </div>
-          </div>
-      ) : null;
-
-      const StarRating = (authen !== undefined && authen && media !== undefined) ? (
+      const StarRating = (media !== undefined && media.avg_rating !== undefined) ? (
           <div className='rate'>
+              {console.log('hello there')}
               <input type='radio' id='star5' name='rate' value='5' checked={Math.round(media.avg_rating) === 5 || onclick} />
               <label htmlFor='star5' title='text'>5 stars</label>
               <input type='radio' id='star4' name='rate' value='4' checked={Math.round(media.avg_rating) === 4 || onclick} />
@@ -101,42 +92,65 @@ export class Media extends Component {
               <label htmlFor='star1' title='text'>1 star</label>
           </div>
       ) : null;
-
+      const genreInfo = (media !== undefined && media.genres !== undefined) ? media.genres.map(genre => (
+          <div className='row' style={{ margin: 'auto' }}>
+              <p>{genre}</p>
+          </div>
+      )) : null;
+      const starInfo = (media !== undefined && media.genres !== undefined) ? media.stars.map(star => (
+          <div className='row' style={{ margin: 'auto' }}>
+              <p>{star}</p>
+          </div>
+      )) : null;
       const error = mediaError !== undefined ? <h1>Error</h1> : null;
-
       const mediaElems = media !== undefined ? (
           <div className='mediaBody'>
-              <ReactPlayer className='media-box' url={authen ? 'https://s3.amazonaws.com/videovault4800/movies/Bird+Box.mp4' : 'https://youtu.be/Kxms-OtUXS0'} controls />
+              {console.log(media)}
+              <div className='player-wrapper'>
+                  <ReactPlayer className='react-player' url='https://s3.amazonaws.com/videovault4800/movies/Bird+Box.mp4' wide='100%' height='100%' controls />
+              </div>
               {media.season_info !== undefined && <div id='overflowBox'>{seasonInfo}</div>}
               <h1>
                   {media.title || title}
+                  {' '}
                   <span>
-                    (
+(
                       {media.year}
-                    )
+)
 
                   </span>
               </h1>
               {media.season_info !== undefined && <h3>SEASONS</h3>}
 
               <img src={media.image_url} alt='Cover art' className='boxArt' />
-              {media.season_info === undefined && authen && <Button color='danger' className='rent-button'>Rent</Button>}
-              {media.season_info !== undefined && authen && <Button color='danger' className='subscribe-button'>Subscribe</Button>}
+              {media.season_info === undefined && <Button color='danger' className='rent-button'>Rent</Button>}
+              {media.season_info !== undefined && <Button color='danger' className='subscribe-button'>Subscribe</Button>}
               <div className='container'>
                   <div className='media-info'>
-                      <h5>
-                        RATING :
-                          {media.avg_rating !== null ? media.avg_rating : 'NO RATINGS'}
-                      </h5>
-                      <h5>SYNOPSIS</h5>
+                      <div className='starsLine'>
+                          <h2>STARS:</h2>
+                          <div className='starsPosition'>{starInfo}</div>
+                      </div>
+                      <div className='genreLine'>
+                          <h2>Genres: </h2>
+                          <div className='genresPosition'>{genreInfo}</div>
+                      </div>
+                      <h2>SYNOPSIS</h2>
                       <p>{media.description}</p>
                   </div>
               </div>
-              {commentContainer}
+              <div className='comment-container'>
+                  <div className='comment-header'>
+                      <label htmlFor='Comment' style={{ textDecoration: 'underline', fontFamily: 'Apple Chancery, cursive' }}>Leave a comment</label>
+                  </div>
+                  <textarea id='subject' name='subject' placeholder='Enter your comment here...' style={{ borderStyle: 'inset', width: '600px', height: '90px' }} />
+                  <div className='row'>
+                      <input type='submit' value='Post Comment' />
+                  </div>
+              </div>
               {StarRating}
           </div>
       ) : null;
-
 
       return (
           <body className='background-color'>
