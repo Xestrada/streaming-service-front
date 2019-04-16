adb5c8ddddefa44ffdd8529c8a233ec95b5e70a5
Jorge Estrada <Xestrada.Jorge@gmail.com>
2019-04-16 09:40:12 -0700

    Created proper profile and subs page

--- src/features/profile/profile.js
+++ src/features/profile/profile.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../common/redux/actions';
 import * as profileActions from './redux/actions';
 import Header from '../common/header';
 import Footer from '../common/footer';
+import ContentBox from '../common/contenBox';
+import Rating from '../common/rating';
 import TimelinePost from '../common/timelinePost';
+import emptyImg from '../../images/noimage.png';
 import './profile.scss';
 
 class Profile extends Component {
@@ -15,17 +19,47 @@ class Profile extends Component {
         common: PropTypes.object.isRequired,
         profileActions: PropTypes.object.isRequired,
         profile: PropTypes.object.isRequired,
+        actions: PropTypes.object.isRequired,
     };
 
     constructor(props) {
         super(props);
 
         this.getWall = this.getWall.bind(this);
+        this.getRatedMovies = this.getRatedMovies.bind(this);
+        this.getRatedTV = this.getRatedTV.bind(this);
+        this.getUserFriends = this.getUserFriends.bind(this);
     }
 
     componentDidMount() {
+        const { common } = this.props;
+        const { authen } = common;
+        if (authen) {
+            this.getRatedMovies();
+            this.getRatedTV();
+            this.getUserFriends();
+        }
+    }
 
+    getRatedMovies() {
+        const { actions, common } = this.props;
+        const { ratedMovies } = actions;
+        const { userData } = common;
+        ratedMovies(userData.id);
+    }
 
+    getRatedTV() {
+        const { actions, common } = this.props;
+        const { ratedTv } = actions;
+        const { userData } = common;
+        ratedTv(userData.id);
+    }
+
+    getUserFriends() {
+        const { actions, common } = this.props;
+        const { getFriends } = actions;
+        const { userData } = common;
+        getFriends(userData.id);
     }
 
     getWall() {
@@ -35,12 +69,22 @@ class Profile extends Component {
         getWall(userData.id);
     }
 
+
     render() {
         const { common, profile } = this.props;
-        const { authen, userData } = common;
+        const {
+            authen,
+            friends,
+            getFriendsPending,
+            ratedMovies,
+            ratedTV,
+            ratedTvPending,
+            ratedMoviesPending,
+
+        } = common;
         const { wall, getWallError, getWallPending } = profile;
 
-        if (authen && wall === undefined && userData !== undefined && !getWallPending && (getWallError === null || getWallError === undefined)) {
+        if (authen && wall === undefined && !getWallPending && (getWallError === null || getWallError === undefined)) {
             this.getWall();
         }
 
@@ -50,12 +94,66 @@ class Profile extends Component {
             </div>
         )) : null;
 
+        const ratedMoviesList = (ratedMovies !== undefined) ? ratedMovies.map(movie => (
+            <div>
+                <div>
+                    <ContentBox title={movie.movie_title} url={`/media/${movie.movie_title}`} image={movie.image_url || emptyImg} />
+                </div>
+                <div>
+                    <Rating rating={movie.rating} />
+                </div>
+            </div>
+        )) : null;
+
+        const ratedTVList = (ratedTV !== undefined) ? ratedTV.map(content => (
+            <div>
+                <div>
+                    <ContentBox title={content.tv_show_title} url={`/media/${content.tv_show_title}`} image={content.image_url || emptyImg} />
+                </div>
+                <div>
+                    <Rating rating={content.rating} />
+                </div>
+            </div>
+        )) : null;
+
+        const friendsList = friends !== undefined ? friends.map(friend => (
+            <ContentBox title={friend.username} url={`/user/${friend.username}/${friend.id}`} image={emptyImg} />
+        )) : null;
+
+        const redir = authen ? null : (<Redirect to='/' />);
+
+        const empty = (<h2>No Content</h2>);
+
+        const loading = <i className='fa fa-spinner fa-spin loadIcon loadingSpinner' />;
+
         return (
             <div className='background'>
                 <Header />
+                {redir}
                 <div className='gridContainer'>
                     <div>
+                        <h1>Wall</h1>
                         {postElems}
+                    </div>
+                    <div className='gridContainer'>
+                        <h1>Friends</h1>
+                        <div className='section'>
+                            {getFriendsPending ? loading : (friendsList || empty)}
+                        </div>
+                    </div>
+
+                    <div className='gridContainer'>
+                        <h1>Rated Movies</h1>
+                        <div className='section'>
+                            {ratedMoviesPending ? loading : (ratedMoviesList || empty)}
+                        </div>
+                    </div>
+
+                    <div className='gridContainer'>
+                        <h1>Rated TV Shows</h1>
+                        <div className='section'>
+                            {ratedTvPending ? loading : (ratedTVList || empty)}
+                        </div>
                     </div>
                 </div>
                 <Footer />
