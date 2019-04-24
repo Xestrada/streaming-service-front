6f6c74fd64f2823f38412e6690b20475fe97f923
Willie Lan <willie1219@gmail.com>
2019-04-24 10:44:51 -0700

    Fixed and added tests for movies, ratedMovies, ratedTv, subs, and getWall

--- tests/features/common/redux/subs.test.js
+++ tests/features/common/redux/subs.test.js
@@ -3,95 +3,142 @@ import thunk from 'redux-thunk';
 import nock from 'nock';
 
 import {
-  COMMON_SUBS_BEGIN,
-  COMMON_SUBS_SUCCESS,
-  COMMON_SUBS_FAILURE,
-  COMMON_SUBS_DISMISS_ERROR,
+    COMMON_SUBS_BEGIN,
+    COMMON_SUBS_SUCCESS,
+    COMMON_SUBS_FAILURE,
+    COMMON_SUBS_DISMISS_ERROR,
 } from '../../../../src/features/common/redux/constants';
 
 import {
-  subs,
-  dismissSubsError,
-  reducer,
+    getSubs,
+    dismissSubsError,
+    reducer,
 } from '../../../../src/features/common/redux/subs';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('common/redux/subs', () => {
-  afterEach(() => {
-    nock.cleanAll();
-  });
+    afterEach(() => {
+        nock.cleanAll();
+    });
 
-  it('dispatches success action when subs succeeds', () => {
-    const store = mockStore({});
+    it('dispatches success action when subs succeeds', () => {
+        const store = mockStore({});
 
-    return store.dispatch(subs())
-      .then(() => {
-        const actions = store.getActions();
-        expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
-        expect(actions[1]).toHaveProperty('type', COMMON_SUBS_SUCCESS);
-      });
-  });
+        return store.dispatch(getSubs(1))
+            .then(() => {
+                const actions = store.getActions();
+                expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
+                expect(actions[1]).toHaveProperty('type', COMMON_SUBS_SUCCESS);
+                expect(actions[1]).toHaveProperty('data', expect.anything());
+            });
+    });
 
-  it('dispatches failure action when subs fails', () => {
-    const store = mockStore({});
+    it('dispatches success action when subs succeeds', () => {
+        const store = mockStore({});
 
-    return store.dispatch(subs({ error: true }))
-      .catch(() => {
-        const actions = store.getActions();
-        expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
-        expect(actions[1]).toHaveProperty('type', COMMON_SUBS_FAILURE);
-        expect(actions[1]).toHaveProperty('data.error', expect.anything());
-      });
-  });
+        return store.dispatch(getSubs(2))
+            .then(() => {
+                const actions = store.getActions();
+                expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
+                expect(actions[1]).toHaveProperty('type', COMMON_SUBS_SUCCESS);
+                expect(actions[1]).toHaveProperty('data', expect.anything());
+            });
+    });
 
-  it('returns correct action by dismissSubsError', () => {
-    const expectedAction = {
-      type: COMMON_SUBS_DISMISS_ERROR,
-    };
-    expect(dismissSubsError()).toEqual(expectedAction);
-  });
+    it('dispatches failure action when subs fails with empty parameter', () => {
+        const store = mockStore({});
 
-  it('handles action type COMMON_SUBS_BEGIN correctly', () => {
-    const prevState = { subsPending: false };
-    const state = reducer(
-      prevState,
-      { type: COMMON_SUBS_BEGIN }
-    );
-    expect(state).not.toBe(prevState); // should be immutable
-    expect(state.subsPending).toBe(true);
-  });
+        return store.dispatch(getSubs())
+            .catch(() => {
+                const actions = store.getActions();
+                expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
+                expect(actions[1]).toHaveProperty('type', COMMON_SUBS_FAILURE);
+                expect(actions[1]).toHaveProperty('error', expect.anything());
+            });
+    });
 
-  it('handles action type COMMON_SUBS_SUCCESS correctly', () => {
-    const prevState = { subsPending: true };
-    const state = reducer(
-      prevState,
-      { type: COMMON_SUBS_SUCCESS, data: {} }
-    );
-    expect(state).not.toBe(prevState); // should be immutable
-    expect(state.subsPending).toBe(false);
-  });
+    it('dispatches failure action when subs fails with negative id', () => {
+        const store = mockStore({});
 
-  it('handles action type COMMON_SUBS_FAILURE correctly', () => {
-    const prevState = { subsPending: true };
-    const state = reducer(
-      prevState,
-      { type: COMMON_SUBS_FAILURE, data: { error: new Error('some error') } }
-    );
-    expect(state).not.toBe(prevState); // should be immutable
-    expect(state.subsPending).toBe(false);
-    expect(state.subsError).toEqual(expect.anything());
-  });
+        return store.dispatch(getSubs(-1))
+            .catch(() => {
+                const actions = store.getActions();
+                expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
+                expect(actions[1]).toHaveProperty('type', COMMON_SUBS_FAILURE);
+                expect(actions[1]).toHaveProperty('error', expect.anything());
+            });
+    });
 
-  it('handles action type COMMON_SUBS_DISMISS_ERROR correctly', () => {
-    const prevState = { subsError: new Error('some error') };
-    const state = reducer(
-      prevState,
-      { type: COMMON_SUBS_DISMISS_ERROR }
-    );
-    expect(state).not.toBe(prevState); // should be immutable
-    expect(state.subsError).toBe(null);
-  });
+    it('dispatches failure action when subs fails with string', () => {
+        const store = mockStore({});
+
+        return store.dispatch(getSubs(''))
+            .catch(() => {
+                const actions = store.getActions();
+                expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
+                expect(actions[1]).toHaveProperty('type', COMMON_SUBS_FAILURE);
+                expect(actions[1]).toHaveProperty('error', expect.anything());
+            });
+    });
+    it('dispatches failure action when subs fails', () => {
+        const store = mockStore({});
+
+        return store.dispatch(getSubs({ error: true }))
+            .catch(() => {
+                const actions = store.getActions();
+                expect(actions[0]).toHaveProperty('type', COMMON_SUBS_BEGIN);
+                expect(actions[1]).toHaveProperty('type', COMMON_SUBS_FAILURE);
+                expect(actions[1]).toHaveProperty('data.error', expect.anything());
+            });
+    });
+
+    it('returns correct action by dismissSubsError', () => {
+        const expectedAction = {
+            type: COMMON_SUBS_DISMISS_ERROR,
+        };
+        expect(dismissSubsError()).toEqual(expectedAction);
+    });
+
+    it('handles action type COMMON_SUBS_BEGIN correctly', () => {
+        const prevState = { subsPending: false };
+        const state = reducer(
+            prevState,
+            { type: COMMON_SUBS_BEGIN },
+        );
+        expect(state).not.toBe(prevState); // should be immutable
+        expect(state.subsPending).toBe(true);
+    });
+
+    it('handles action type COMMON_SUBS_SUCCESS correctly', () => {
+        const prevState = { subsPending: true };
+        const state = reducer(
+            prevState,
+            { type: COMMON_SUBS_SUCCESS, data: {} },
+        );
+        expect(state).not.toBe(prevState); // should be immutable
+        expect(state.subsPending).toBe(false);
+    });
+
+    it('handles action type COMMON_SUBS_FAILURE correctly', () => {
+        const prevState = { subsPending: true };
+        const state = reducer(
+            prevState,
+            { type: COMMON_SUBS_FAILURE, data: { error: new Error('some error') } },
+        );
+        expect(state).not.toBe(prevState); // should be immutable
+        expect(state.subsPending).toBe(false);
+        expect(state.subsError).toEqual(expect.anything());
+    });
+
+    it('handles action type COMMON_SUBS_DISMISS_ERROR correctly', () => {
+        const prevState = { subsError: new Error('some error') };
+        const state = reducer(
+            prevState,
+            { type: COMMON_SUBS_DISMISS_ERROR },
+        );
+        expect(state).not.toBe(prevState); // should be immutable
+        expect(state.subsError).toBe(null);
+    });
 });
-
